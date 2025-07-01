c522c6173614f5f12a55f4ecfba864a4075d4c42
Anisul Haque Niloy <niloy2931@gmail.com>
2025-07-01 11:16:33 +0600

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,6 @@
+const User = require('../model/User');
+const generateToken = require('../utils/generateToken');
+
 exports.register = async (req, res) => {
     const { name, email, password, photoURL } = req.body;
     try {
@@ -5,16 +8,12 @@ exports.register = async (req, res) => {
         if (userExists) return res.status(400).json({ message: 'User already exists' });
 
         const user = await User.create({ name, email, password, photoURL });
-
         res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            photoURL: user.photoURL,
             token: generateToken(user._id),
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                photoURL: user.photoURL,
-                role: user.role || "user", // optional
-            }
         });
     } catch (err) {
         res.status(500).json({ message: 'Server error' });
@@ -29,15 +28,52 @@ exports.login = async (req, res) => {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        res.status(200).json({
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            photoURL: user.photoURL,
             token: generateToken(user._id),
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                photoURL: user.photoURL,
-                role: user.role || "user", // optional
-            }
+        });
+    } catch (err) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};const User = require('../model/User');
+const generateToken = require('../utils/generateToken');
+
+exports.register = async (req, res) => {
+    const { name, email, password, photoURL } = req.body;
+    try {
+        const userExists = await User.findOne({ email });
+        if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+        const user = await User.create({ name, email, password, photoURL });
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            photoURL: user.photoURL,
+            token: generateToken(user._id),
+        });
+    } catch (err) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user || !(await user.matchPassword(password))) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            photoURL: user.photoURL,
+            token: generateToken(user._id),
         });
     } catch (err) {
         res.status(500).json({ message: 'Server error' });
